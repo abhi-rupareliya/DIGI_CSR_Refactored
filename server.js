847579987f44a1e3386e4e798978114034eba65c
Abhi Rupareliya <96171169+abhi-rupareliya@users.noreply.github.com>
2024-02-12 22:07:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const logger = require("morgan"); // for logging requests , status codes and mor
 const app = express();
 const PORT = process.env.PORT;
 const { router } = require("./Routes/index");
-const seeder = require("./Seeders/CRN_seeders");
+
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(express.json({ limit: "10mb" }));
 
 // Serve static files from the 'uploads' folder -- temporary
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(logger("dev"));
-// seeder.seedCRN();
+
 router(app);
 app.listen(PORT, () => {
   console.log(`Server listening on : http://localhost:${PORT}`);
